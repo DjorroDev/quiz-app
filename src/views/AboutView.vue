688a261bbd9cbd49a274b457ae9c2582088ef28d
DjorroDev <aufarmawarid@gmail.com>
2022-08-11 23:35:09 +0700

    Fix about page, link source tidak berfungsi

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <h1>about Page</h1>
     <p>Quiz app for fun</p>
-    <el-button size="large" type="success" link="https://github.com/DjorroDev/quiz-app.git"
-      >Star The Repository in here!</el-button
-    >
-    <el-button size="large" type="primary" link="https://github.com/DjorroDev"
-      >Follow me in Github!</el-button
-    >
+    <el-divider />
+    <a target="_blank" href="https://github.com/DjorroDev/quiz-app.git">
+      <el-button size="large" class="about-btn" type="success" link
+        >Star The Repository in here! ✨</el-button
+      >
+    </a>
+    <a target="_blank" href="https://github.com/DjorroDev">
+      <el-button class="about-btn" size="large" type="primary" link>Follow me on Github!</el-button>
+    </a>
   </div>
 </template>
 
-<!-- <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style>
+a {
+  display: block;
+  margin-bottom: 10px;
 }
-</style> -->
+
+.about-btn {
+  font-size: large;
+}
+</style>
