ad0fe6ed007c9b629960e26b3baddcd4e98863df
DjorroDev <aufarmawarid@gmail.com>
2022-08-24 23:36:29 +0700

    Improve navbar and added login and register

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,17 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { UserFilled, Menu, Tickets, Plus } from "@element-plus/icons-vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+console.log(route.path);
+
+const urlPath = ref(route.path);
+
+watch(urlPath, async (newPath, oldPath) => {
+  console.log(newPath);
+  console.log(oldPath);
+});
 
 const activeIndex = ref("/");
 </script>
@@ -13,10 +25,41 @@ const activeIndex = ref("/");
     :ellipsis="false"
     background-color=""
   >
-    <el-menu-item index="/">LOGO</el-menu-item>
+    <!-- <el-dropdown size="large">
+      <el-icon class="menu-icon" :size="25"><Menu /></el-icon>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>Login</el-dropdown-item>
+          <el-dropdown-item>Register</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown> -->
+    <el-menu-item index="/">
+      <el-icon :size="35"><Tickets /></el-icon>
+      QUIZ
+    </el-menu-item>
     <div class="flex-grow"></div>
-    <el-menu-item index="/create">Create Quiz</el-menu-item>
-    <el-menu-item index="/about">About</el-menu-item>
+    <el-menu-item class="menu-item" index="/create">
+      <el-icon><Plus /></el-icon>Create Quiz</el-menu-item
+    >
+    <el-menu-item class="menu-item" index="/about">About</el-menu-item>
+    <el-sub-menu class="menu-item-mobile" index="1">
+      <template #title>
+        <el-icon :size="25"><Menu /></el-icon>
+        Menu
+      </template>
+      <el-menu-item index="/create"
+        ><el-icon><Plus /></el-icon>Create Quiz</el-menu-item
+      >
+      <el-menu-item index="/about">About Page</el-menu-item>
+    </el-sub-menu>
+    <el-sub-menu index="2">
+      <template #title>
+        <el-icon :size="25"><UserFilled /></el-icon>
+      </template>
+      <el-menu-item index="/login">Login</el-menu-item>
+      <el-menu-item index="/register">Register</el-menu-item>
+    </el-sub-menu>
   </el-menu>
 </template>
 
@@ -33,13 +76,34 @@ header {
   padding: 0 0 0 0;
 }
 
+.menu-item-mobile {
+  display: none;
+}
+
+.menu-icon {
+  margin: auto;
+  margin-left: 15px;
+  margin-right: 15px;
+}
+
 .el-menu {
   padding: 0 40px 0 40px;
 }
 
 @media (max-width: 768px) {
+  .menu-item-mobile {
+    display: flex;
+    padding: 0;
+  }
+  .menu-item {
+    display: none;
+  }
   .el-menu {
     padding: 0 0 0 0;
+  }
+
+  .el-avatar {
+    margin-right: 10px;
   }
 }
 </style>
